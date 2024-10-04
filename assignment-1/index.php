04b74faeaffca8a6e11eefeb2802e559200a5834
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2024-10-04 23:48:43 +0800

    header-footer

---
+++ assignment-1/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotify - Web Player: Music for Everyone</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="assets/images/spotify-icon.png">
+</head>
+
+<body>
+    <header>
+        <a href="index.php"><img src="assets/images/spotify-icon.png" alt="icon"></a>
+        <div>
+            <a href="index.php"><img src="assets/images/home-icon-focus.png" alt="icon"></a>
+            <a class="search">
+                <div>
+                    <img src="assets/images/search-icon.png" alt="icon" title="Search">
+                    <input type="text" placeholder="What do you want to play?">
+                </div>
+                <div>
+                    |
+                    <img src="assets/images/browse-icon.png" alt="">
+                </div>
+            </a>
+        </div>
+        <div>
+            <a href="pages/login.php">Sign Up</a>
+            <a href="pages/signup.php">Log In</a>
+        </div>
+    </header>
+
+    <section>
+       <div>
+        <h1><img src="" alt="library icon"></h1>
+       </div>
+       <div></div>
+    </section>
+
+    <footer>
+        <a href="pages/signup.php">
+            <p><strong>Preview of Spotify</strong><br>Sign up to get unlimited songs and podcast with occasional ads. No credit card needed.</p>
+            <button> Sign up free </button>
+        </a>
+    </footer>
+</body>
+
+</html>
