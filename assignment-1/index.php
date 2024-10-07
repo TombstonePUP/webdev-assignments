59f649724847a1fec01b150bc129a4bf66729b86
1101101011 <ilardecharles@gmail.com>
2024-10-07 14:09:53 +0800

    Katangahan sa home page

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -2,48 +2,74 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spotify - Web Player: Music for Everyone</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="icon" href="assets/images/spotify-icon.png">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Spotify - Web Player: Music for Everyone</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="icon" href="assets/images/spotify-icon.png">
 </head>
 
 <body>
-    <header>
-        <a id="main-icon" href="index.php"><img src="assets/images/spotify-icon.png" alt="icon"></a>
+  <header>
+    <a id="main-icon" href="index.php"><img src="assets/images/spotify-icon.png" alt="icon"></a>
+    <div>
+      <a href="index.php"><img src="assets/images/home-icon-focus.png" alt="icon"></a>
+      <a class="search">
         <div>
-            <a href="index.php"><img src="assets/images/home-icon-focus.png" alt="icon"></a>
-            <a class="search">
-                <div>
-                    <img src="assets/images/search-icon.png" alt="icon" title="Search">
-                    <input type="text" placeholder="What do you want to play?">
-                </div>
-                <div>
-                    |
-                    <img src="assets/images/browse-icon.png" alt="">
-                </div>
-            </a>
+          <img src="assets/images/search-icon.png" alt="icon" title="Search">
+          <input type="text" placeholder="What do you want to play?">
         </div>
         <div>
-            <a href="pages/signup.php">Sign Up</a>
-            <a href="pages/login.php">Log In</a>
+          |
+          <img src="assets/images/browse-icon.png" alt="">
         </div>
-    </header>
+      </a>
+    </div>
+    <div>
+      <a href="pages/signup.php">Sign Up</a>
+      <a href="pages/login.php">Log In</a>
+    </div>
+  </header>
 
-    <section>
+  <section class="index-container">
+    <div>
+      <div>
+        <img src="assets/images/library.png" alt="">
+        <p>Your Library</p>
+        <img src="assets/images/plus.png" alt="">
+      </div> <!-- <h1><img src="" alt="library icon"></h1> -->
+      <div>
         <div>
-            <!-- <h1><img src="" alt="library icon"></h1> -->
+          <a>Create your first playlist</a>
+          <a>It's easy, we'll help you.</a>
+          <a>Create Playlist</a>
         </div>
-        <div></div>
-    </section>
+        <div>
+          <a>Let's find some podcasts to follow</a>
+          <a>We'll keep you updated on new episodes</a>
+          <a>Browse podcasts</a>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
+    <div>
+      <p>Popular artists</p>
+      <div class="index-grid-container">
+        <div class="index-category">
+          <a>Music</a>
+          <img src="assets/images/400x400.svg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <footer>
-        <a href="pages/signup.php">
-            <p><strong>Preview of Spotify</strong><br>Sign up to get unlimited songs and podcast with occasional ads. No credit card needed.</p>
-            <button> Sign up free </button>
-        </a>
-    </footer>
+  <footer>
+    <a href="pages/signup.php">
+      <p><strong>Preview of Spotify</strong><br>Sign up to get unlimited songs and podcast with occasional ads. No credit card needed.</p>
+      <button> Sign up free </button>
+    </a>
+  </footer>
 </body>
 
 </html>
